b63cf036df0d67ea954cd9727ff25a6ef722ab72
Nguyen-Huu-Hai-Phuoc <hinobai123@gmail.com>
2022-08-21 21:27:09 +0700

    font-end

---
+++ js/students.js
@@ -0,0 +1,32 @@
+[
+  {
+    "username": "phuocnhh",
+    "password": "123",
+    "fullname": "Nguyen Huu Hai Phuoc",
+    "email": "phuoc@gamil.com",
+    "gender": "Nam",
+    "birthday": "2002-07-02",
+    "schoolfee": "1500000",
+    "marks": "0"
+  },
+  {
+    "username": "pheonv",
+    "password": "iloveyou",
+    "fullname": "Nguyễn Văn Chí Phèo",
+    "email": "pheonv@fpt.edu.vn",
+    "gender": "Nam",
+    "birthday": "1985-10-11",
+    "schoolfee": "2500000",
+    "marks": "0"
+  },
+  {
+    "username": "nopt",
+    "password": "iloveyou",
+    "fullname": "Phạm Thị Nở",
+    "email": "nopt@fpt.edu.vn",
+    "gender": "Nu",
+    "birthday": "1993-05-15",
+    "schoolfee": "2000000",
+    "marks": "0"
+  }
+]
